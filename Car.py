1b04978db38d36a989cfc6fff06e5d142ff0b236
yubinx <yubinxie222@gmail.com>
2025-03-02 20:06:33 -0500

    Create a toString method

--- Car.py
+++ Car.py
@@ -1,11 +1,14 @@
 class Car:
-    number_of_wheels
-    brand_name
-    mileage
-    price
+    number_of_wheels = None
+    brand_name       = None
+    mileage          = None
+    price            = None
 
     def __init__(self, number_of_wheels, brand_name, mileage, price):
         self.number_of_wheels = number_of_wheels
         self.brand_name = brand_name
         self.mileage = mileage
         self.price = price 
+    
+    def __str__(self):
+        return "Car: " + self.brand_name + " has " + str(self.number_of_wheels) + " wheels, mileage is " + str(self.mileage) + " and price is " + str(self.price)
---
+++ Test.py
@@ -0,0 +1,9 @@
+from Car import Car
+
+Toyota = Car(4, "Toyota", 12, 10000)
+
+print(Toyota.number_of_wheels)
+print(Toyota.brand_name)
+print(Toyota.mileage)
+print(Toyota.price)
+print(Toyota)
