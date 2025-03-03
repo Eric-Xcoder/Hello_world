0c620284b22289d778e39f5bff81670705f95344
yubinx <yubinxie222@gmail.com>
2025-03-02 19:53:58 -0500

    Create a Car Class

---
+++ Car.py
@@ -0,0 +1,8 @@
+class Car:
+    number_of_wheels
+    brand_name
+    mileage
+    price
+
+    def __init__(self, number_of_wheels, brand_name, mileage, price):
+        self.number_of_wheels = number_of_wheels
