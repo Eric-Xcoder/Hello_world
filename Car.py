627aa42ad65f713d5a0b8cbe1daab161563b48a6
Eric-Xcoder <ericztxiao@gmail.com>
2025-03-02 19:58:26 -0500

    addedvariables

--- Car.py
+++ Car.py
@@ -6,3 +6,6 @@ class Car:
 
     def __init__(self, number_of_wheels, brand_name, mileage, price):
         self.number_of_wheels = number_of_wheels
+        self.brand_name = brand_name
+        self.mileage = mileage
+        self.price = price 
